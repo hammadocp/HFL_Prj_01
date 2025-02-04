634e36458f5213f36cf532d3a1db561a431a47e3
Hammad <hammadocp@gmail.com>
2025-02-04 21:00:10 +0400

    Function fetchChannelConfig Updated

--- network.sh
+++ network.sh
@@ -87,41 +87,66 @@ function createChannel(){
 
 function joinChannel(){
 
-
   export CORE_PEER_TLS_ENABLED=true
-  export CORE_PEER_LOCALMSPID=OrgA_MSP
+  export CORE_PEER_LOCALMSPID=OrgAMSP
   export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/orgA.example.com/peers/peer0.orgA.example.com/tls/ca.crt
   export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/orgA.example.com/users/Admin@orgA.example.com/msp
   export CORE_PEER_ADDRESS=localhost:7051
 
  BLOCKFILE="./channel-artifacts/${CHANNEL_NAME}.block"
  peer channel join -b $BLOCKFILE >&log.txt
-
+ peer channel list
   export CORE_PEER_TLS_ENABLED=true
-  export CORE_PEER_LOCALMSPID=OrgB_MSP
+  export CORE_PEER_LOCALMSPID=OrgBMSP
   export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/orgB.example.com/peers/peer0.orgB.example.com/tls/ca.crt
   export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/orgB.example.com/users/Admin@orgB.example.com/msp
   export CORE_PEER_ADDRESS=localhost:9051
 
   BLOCKFILE="./channel-artifacts/${CHANNEL_NAME}.block"
   peer channel join -b $BLOCKFILE >&log2.txt
-
+  peer channel list
   echo "OrgA OrgB proposal submitted successfully!"
- set +x
 }
 
 fetchChannelConfig() {
+
   # Setting Anchor Peers
   export CORE_PEER_TLS_ENABLED=true
-  export CORE_PEER_LOCALMSPID=OrgA_MSP
+  export CORE_PEER_LOCALMSPID=OrgAMSP
   export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/orgA.example.com/peers/peer0.orgA.example.com/tls/ca.crt
   export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/orgA.example.com/users/Admin@orgA.example.com/msp
   export CORE_PEER_ADDRESS=localhost:7051
 
 : ${CHANNEL_NAME:="mychannel"}
+: ${HOST:="peer0.orgA.example.com"}
+: ${PORT:="7051"}
+
   export ORDERER_CA=${PWD}/organizations/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem
   peer channel fetch config ./channel-artifacts/config_block.pb -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com -c "$CHANNEL_NAME" --tls --cafile "$ORDERER_CA"
-  echo $CHANNEL_NAME
+  configtxlator proto_decode --input ${PWD}/channel-artifacts/config_block.pb --type common.Block --output ${PWD}/channel-artifacts/config_block.json 
+  jq .data.data[0].payload.data.config ${PWD}/channel-artifacts/config_block.json >"${PWD}/channel-artifacts/${CORE_PEER_LOCALMSPID}config.json"
+  
+  jq '.channel_group.groups.Application.groups.'${CORE_PEER_LOCALMSPID}'.values += {"AnchorPeers":{"mod_policy": "Admins","value":{"anchor_peers": [{"host": "'$HOST'","port": '$PORT'}]},"version": "0"}}' ${PWD}/channel-artifacts/${CORE_PEER_LOCALMSPID}config.json > ${PWD}/channel-artifacts/${CORE_PEER_LOCALMSPID}modified_config.json
+
+  jq '.channel_group.groups.Application.groups.OrgBMSP.values += {"AnchorPeers":{"mod_policy": "Admins","value":{"anchor_peers": [{"host": "peer0.orgB.example.com","port": 9051}]},"version": "0"}}' ${PWD}/channel-artifacts/${CORE_PEER_LOCALMSPID}modified_config.json > ${PWD}/channel-artifacts/OrgBMSPmodified_config.json
+
+  #configtxlator proto_encode --input "${ORIGINAL}" --type common.Config --output ${PWD}/channel-artifacts/original_config.pb
+  configtxlator proto_encode --input ${PWD}/channel-artifacts/OrgBMSPmodified_config.json --type common.Config --output ${PWD}/channel-artifacts/modified_config.pb
+  configtxlator compute_update --channel_id "${CHANNEL_NAME}" --original ${PWD}/channel-artifacts/config_block.pb --updated ${PWD}/channel-artifacts/modified_config.pb --output ${PWD}/channel-artifacts/config_update.pb
+  configtxlator proto_decode --input ${PWD}/channel-artifacts/config_update.pb --type common.ConfigUpdate --output ${PWD}/channel-artifacts/config_update.json
+  echo '{"payload":{"header":{"channel_header":{"channel_id":"'$CHANNEL_NAME'", "type":2}},"data":{"config_update":'$(cat ${PWD}/channel-artifacts/config_update.json)'}}}' | jq . > ${PWD}/channel-artifacts/config_update_in_envelope.json
+  configtxlator proto_encode --input ${PWD}/channel-artifacts/config_update_in_envelope.json --type common.Envelope --output ${PWD}/channel-artifacts/config_update_envelope.pb
+
+  peer channel signconfigtx -f ${PWD}/channel-artifacts/config_update_envelope.pb
+
+  export CORE_PEER_TLS_ENABLED=true
+  export CORE_PEER_LOCALMSPID=OrgBMSP
+  export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/orgB.example.com/peers/peer0.orgB.example.com/tls/ca.crt
+  export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/orgB.example.com/users/Admin@orgB.example.com/msp
+  export CORE_PEER_ADDRESS=localhost:9051  
+
+  peer channel signconfigtx -f ${PWD}/channel-artifacts/config_update_envelope.pb
+
 }
 
 
